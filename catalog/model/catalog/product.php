7b4c4660452373f2a7a47b9b8d2ff17c26c071e3
VaHHiLLiH <serj.solo1488@gmail.com>
2022-10-03 15:37:29 +0300

    поправил отображение изображений в карточках товаров и изменил логику выборки 4 товаров в блоке товары с похожими опциями

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -541,7 +541,7 @@ class ModelCatalogProduct extends Model {
 
         $parent_id = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_to_category ptc LEFT JOIN " . DB_PREFIX . "category c ON (ptc.category_id = c.category_id) WHERE ptc.product_id = " . $product_id . " AND c.parent_id = 0")->row['category_id'];
 
-        $query = $this->db->query("SELECT `product_id` FROM " . DB_PREFIX . "product_to_category WHERE `category_id` = " . $parent_id . " LIMIT 4")->rows;
+        $query = $this->db->query("SELECT `product_id` FROM " . DB_PREFIX . "product_to_category WHERE product_id > " . $product_id . " AND `category_id` = " . $parent_id . " LIMIT 4")->rows;
         $products = array();
         foreach ($query as $item) {
             $products[] = $this->getProduct($item['product_id']);
