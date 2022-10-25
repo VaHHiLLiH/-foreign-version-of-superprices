78b524db8030c34adc18ed5757427d946c1b0f59
VaHHiLLiH <serj.solo1488@gmail.com>
2022-10-25 09:24:01 +0300

    Дамплю категорию

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -102,8 +102,8 @@ class ControllerProductCategory extends Controller {
         var_dump('Why are you staying?');
 		$category_info = $this->model_catalog_category->getCategory($category_id);
 
+        var_dump('Why are you falling?');die();
 		$data['category_description'] = $this->generateDescription($category_id);
-		var_dump('Why are you falling?');die();
 		if ($category_info) {
 			$this->document->setTitle($this->generateMetaTitle($category_id));
 			$this->document->setDescription($this->generateMetaDescription($category_id));
