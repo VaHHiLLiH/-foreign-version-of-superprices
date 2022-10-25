5af68350cc27e735c08b843a606423fa5dfb6638
VaHHiLLiH <serj.solo1488@gmail.com>
2022-10-25 09:33:16 +0300

    Дамплю категорию

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -371,8 +371,8 @@ class ControllerProductCategory extends Controller {
 			$data['column_left'] = $this->load->controller('common/column_left');
 			$data['column_right'] = $this->load->controller('common/column_right');
 			$data['content_top'] = $this->load->controller('common/content_top');
-            //var_dump('WHY are you falling?');die();
 			//$data['content_bottom'] = $this->load->controller('common/content_bottom');
+            var_dump('WHY are you falling?');die();
 			$data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
 
