48b5d7cc67f5706c84fbdfa5a3f5ab3c693bae72
VaHHiLLiH <serj.solo1488@gmail.com>
2022-10-25 15:03:45 +0300

    Удаляю дубли товаров

---
+++ system/console/RemoveProductsFromId.php
@@ -0,0 +1,81 @@
+<?php
+$path = 'C:/OpenServer/domains/burj/';
+
+// Configuration
+require_once($path . 'config.php');
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+require_once(DIR_SYSTEM . 'helper/general.php');
+
+
+// Database
+$db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+
+$products_ids = [
+    4736,
+    4739,
+    4865,
+    4741,
+    4840,
+    16534,
+    16232,
+    16228,
+    16256,
+    16530,
+    16516,
+    16517,
+    16515,
+    16475,
+    16521,
+    16532,
+    16365,
+    4746,
+    4721,
+    4752,
+    8021,
+    8386,
+    4738,
+    16513,
+    16523,
+    16250,
+    16520,
+    16514,
+    16533,
+    16512,
+    16529,
+    16519,
+    16528,
+    4717,
+    4868,
+    13749,
+];
+
+foreach ($products_ids as $product_id) {
+    removeProduct($db, $product_id);
+}
+
+
+function removeProduct($db, $product_id)
+{
+    $db->query("DELETE FROM " . DB_PREFIX . "product WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_attribute WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_description WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_discount WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_filter WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_image WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_option WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_option_value WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_related WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_related WHERE related_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_reward WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_special WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_to_category WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_to_download WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_to_layout WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_to_store WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "product_recurring WHERE product_id = " . (int)$product_id);
+    $db->query("DELETE FROM " . DB_PREFIX . "review WHERE product_id = '" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "seo_url WHERE query = 'product_id=" . (int)$product_id . "'");
+    $db->query("DELETE FROM " . DB_PREFIX . "coupon_product WHERE product_id = '" . (int)$product_id . "'");
+}
