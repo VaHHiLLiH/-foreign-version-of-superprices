e33aad14c77d8c647a2290be9a32ec6193d03d74
VaHHiLLiH <serj.solo1488@gmail.com>
2022-10-27 16:55:27 +0300

    Убрал токены из скрипта перевода)

--- system/console/TranslateAllProductsCharacteristics.php
+++ system/console/TranslateAllProductsCharacteristics.php
@@ -42,8 +42,8 @@ foreach ($products_chars as $key1 => $product_chars) {
 function translateText($text)
 {
     //return 'Случайное текст';
-    $IAM_TOKEN = 't1.9euelZqYnJSJkM_GzM_KkJPIyJyQlu3rnpWakcuMkoycnYqLnp6JiZSJjI_l8_c_ZRZl-e8fXEw4_t3z938TFGX57x9cTDj-.c6FVZkVgMeSfcip8ukUyHHj-XA00Bk-ZcCHB6N8RJNRNGK4M7q2UgxMO1eea2ITenpiWMKUPHV6D-dD8Q2OzCQ';
-    $folder_id = 'b1gq1kelin7d9rqn3ot0';
+    $IAM_TOKEN = '';
+    $folder_id = '';
     $target_language = 'en';
 
     $url = 'https://translate.api.cloud.yandex.net/translate/v2/translate';
@@ -76,43 +76,3 @@ function translateText($text)
     return $translatedText['translations'][0]['text'];
     //var_dump($translatedText['translations'][0]['text']);die();
 }
-/*--------------Test yandex translator api-----------------*/
-
-//$OAuth-токен = 'y0_AgAAAAA4bYvPAATuwQAAAADQtj01ToJd5tqVSpiBmx-jbbrOYz0H53k';
-//$Iam_TOKEN = 't1.9euelZqYnJSJkM_GzM_KkJPIyJyQlu3rnpWakcuMkoycnYqLnp6JiZSJjI_l8_c_ZRZl-e8fXEw4_t3z938TFGX57x9cTDj-.c6FVZkVgMeSfcip8ukUyHHj-XA00Bk-ZcCHB6N8RJNRNGK4M7q2UgxMO1eea2ITenpiWMKUPHV6D-dD8Q2OzCQ';
-//$catalog_ID = 'b1g2omf1i3p6jmj32lgq';
-
-/*$IAM_TOKEN = 't1.9euelZqUkcyJk8ialcaci8-dmpOSkO3rnpWakcuMkoycnYqLnp6JiZSJjI_l8_cKYn9l-e8ZKw88_t3z90oQfWX57xkrDzz-.tNxPPHPRKkX59gIzmLRi-NrZLbTP5jAFz8uiiEEpaUvV_1FaLzqF-UF_31kqdJr8y4lx0iBuO1-Q4JFxmV-xDQ';
-$folder_id = 'b1gq1kelin7d9rqn3ot0';
-$target_language = 'en';
-$texts = ["я пишу этот текст для того, чтобы понять сколько символов будет влезать в описание продуктов"];
-
-$url = 'https://translate.api.cloud.yandex.net/translate/v2/translate';
-
-$headers = [
-    'Content-Type: application/json',
-    "Authorization: Bearer $IAM_TOKEN"
-];
-
-$post_data = [
-    "targetLanguageCode" => $target_language,
-    "texts" => $texts,
-    "folderId" => $folder_id,
-];
-
-$data_json = json_encode($post_data);
-
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, true);
-
-$result = curl_exec($curl);
-
-curl_close($curl);
-
-var_dump($result);*/
-/*--------------End of testing translator api--------------*/
-
