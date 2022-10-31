1326d3c6024e070c05c068d6d84c835b93bfe54a
VaHHiLLiH <serj.solo1488@gmail.com>
2022-10-31 10:48:20 +0300

    Проверка всех товаров на ошибки в характеристиках

--- system/console/CheckFuckupChars.php
+++ system/console/CheckFuckupChars.php
@@ -12,16 +12,26 @@ require_once(DIR_SYSTEM . 'helper/general.php');
 // Database
 $db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
 
-$products_chars = $db->query("SELECT * FROM " . DB_PREFIX . "spec WHERE language_id = 0 LIMIT 6000, 1000")->rows;
+$countRecords = $db->query("SELECT COUNT(*) as count FROM oc_spec WHERE language_id = 0")->row['count'];
+$recordOnOnePage = 1000;
+$currentPage = 1;
+$maxPage = ceil($countRecords/$recordOnOnePage);
+
 var_dump('start');
-foreach ($products_chars as $key1 => $product_chars) {
-    $characteristics = json_decode($product_chars['spec'], true);
-    foreach ($characteristics as $group_chars) {
-        if (empty($group_chars['name'])) {
-            var_dump($products_chars[$key1]['product_id']);
+while ($currentPage <= $maxPage) {
+
+    $from = ($currentPage-1)*$recordOnOnePage;
+
+    $products_chars = $db->query("SELECT * FROM " . DB_PREFIX . "spec WHERE language_id = 0 LIMIT " . $from . ", " . $recordOnOnePage)->rows;
+    foreach ($products_chars as $key1 => $product_chars) {
+        $characteristics = json_decode($product_chars['spec'], true);
+        foreach ($characteristics as $group_chars) {
+            if (empty($group_chars['name'])) {
+                var_dump($products_chars[$key1]['product_id']);
+            }
         }
+        //$db->query("INSERT INTO " . DB_PREFIX . "spec (product_id, language_id, spec) VALUES (" . (int)$products_chars[$key1]['product_id'] . ", 2, '" . $db->escape($products_chars[$key1]['spec']) . "')");
     }
-    //$db->query("INSERT INTO " . DB_PREFIX . "spec (product_id, language_id, spec) VALUES (" . (int)$products_chars[$key1]['product_id'] . ", 2, '" . $db->escape($products_chars[$key1]['spec']) . "')");
 }
 var_dump('end');
 
