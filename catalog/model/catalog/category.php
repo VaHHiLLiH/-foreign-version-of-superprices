122b2ad4e7299ecedb4689f3c79979e2b0d0a13e
VaHHiLLiH <serj.solo1488@gmail.com>
2022-11-07 12:14:06 +0300

    Беру характеристики на английском

--- catalog/model/catalog/category.php
+++ catalog/model/catalog/category.php
@@ -116,14 +116,14 @@ class ModelCatalogCategory extends Model {
 
     public function getProductsChars($category_id, $from, $offset)
     {
-        $query = $this->db->query("SELECT spec FROM " . DB_PREFIX . "spec sp LEFT JOIN " . DB_PREFIX . "product_to_category ptc ON (sp.product_id = ptc.product_id) WHERE ptc.category_id = " . (int)$category_id . " LIMIT " . (int)$from . ", " . (int)$offset)->rows;
+        $query = $this->db->query("SELECT spec FROM " . DB_PREFIX . "spec sp LEFT JOIN " . DB_PREFIX . "product_to_category ptc ON (sp.product_id = ptc.product_id) WHERE sp.language_id = 2 AND ptc.category_id = " . (int)$category_id . " LIMIT " . (int)$from . ", " . (int)$offset)->rows;
 
         return $query;
     }
 
     public function getCountChars($category_id)
     {
-        $query = $this->db->query("SELECT COUNT(*) as count FROM " . DB_PREFIX . "spec sp LEFT JOIN " . DB_PREFIX . "product_to_category ptc ON (sp.product_id = ptc.product_id) WHERE ptc.category_id = " . (int)$category_id)->row;
+        $query = $this->db->query("SELECT COUNT(*) as count FROM " . DB_PREFIX . "spec sp LEFT JOIN " . DB_PREFIX . "product_to_category ptc ON (sp.product_id = ptc.product_id) WHERE sp.language_id = 2 AND ptc.category_id = " . (int)$category_id)->row;
 
         return $query['count'];
     }
