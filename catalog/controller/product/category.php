78f33583caa091a3053652f525ac397466d05b62
VaHHiLLiH <serj.solo1488@gmail.com>
2022-11-08 10:32:16 +0300

    каноникл

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -350,10 +350,10 @@ class ControllerProductCategory extends Controller {
 			$data['results'] = sprintf($this->language->get('text_pagination'), ($product_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($product_total - $limit)) ? $product_total : ((($page - 1) * $limit) + $limit), $product_total, ceil($product_total / $limit));
 
 			// http://googlewebmastercentral.blogspot.com/2011/09/pagination-with-relnext-and-relprev.html
-			if ((int)$page > 1) {
+			//if ((int)$page > 1) {
                 var_dump((int)$page);
 			    $this->document->addLink($this->url->link('product/category', 'path=' . $category_info['category_id']), 'canonical');
-			}
+			//}
 			
 			if ($page > 1) {
 			    $this->document->addLink($this->url->link('product/category', 'path=' . $category_info['category_id'] . (($page - 2) ? '&page='. ($page - 1) : '')), 'prev');
