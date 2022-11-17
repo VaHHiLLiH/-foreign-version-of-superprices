ea25838225f45fa15c6c31f9165b6af5b622e8bb
VaHHiLLiH <serj.solo1488@gmail.com>
2022-11-17 15:18:39 +0300

    отладка

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -546,6 +546,7 @@ class ModelCatalogProduct extends Model {
         foreach ($query as $item) {
             $products[] = $this->getProduct($item['product_id']);
         }
+        echo'<br/><br/>';var_dump($products);echo'<br/><br/>';
         if (count($query) < 4) {
             $limit = 4 - count($query);
             $query2 = $this->db->query("SELECT product_id FROM " . DB_PREFIX . "product_to_category WHERE product_id > " . $product_id . " AND category_id = " . $parent_id . " LIMIT " . $limit)->rows;
