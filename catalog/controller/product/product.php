20606c573ea364b4c34ab99797981bd265fa7280
VaHHiLLiH <serj.solo1488@gmail.com>
2022-11-17 16:53:59 +0300

    убрал каноникал с товара

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -64,7 +64,7 @@ class ControllerProductProduct extends Controller {
 
 
 			$this->document->setKeywords($product_info['meta_keyword']);
-			$this->document->addLink($this->url->link('product/product', 'product_id=' . $this->request->get['product_id']), 'canonical');
+			//$this->document->addLink($this->url->link('product/product', 'product_id=' . $this->request->get['product_id']), 'canonical');
 			$this->document->addScript('catalog/view/javascript/jquery/magnific/jquery.magnific-popup.min.js');
 			$this->document->addStyle('catalog/view/javascript/jquery/magnific/magnific-popup.css');
 			$this->document->addScript('catalog/view/javascript/jquery/datetimepicker/moment/moment.min.js');
