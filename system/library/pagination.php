8fdf0dea95a0a1fd638b47f7cb2cd9828e4000ce
VaHHiLLiH <serj.solo1488@gmail.com>
2022-11-23 12:59:30 +0300

    фикс пагинации

--- system/library/pagination.php
+++ system/library/pagination.php
@@ -85,11 +85,7 @@ class Pagination {
 					if ($i === 1) {
 						$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '?page={page}', '&page={page}', '/page-{page}'), '', $this->url) . '">' . $i . '</a></li>';
 					} else {
-                        if ($i === 1) {
-                            $output .= str_replace('/page-1', '', '<li><a href="' . $this->url . '">' . $i . '</a></li>');
-                        } else {
-                            $output .= '<li><a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a></li>';
-                        }
+						$output .= '<li><a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a></li>';
 					}
 				}
 			}
@@ -101,6 +97,8 @@ class Pagination {
 		}
 
 		$output .= '</ul>';
+
+        str_replace('/page-1"', '"', $output);
 
 		if ($num_pages > 1) {
 			return $output;
