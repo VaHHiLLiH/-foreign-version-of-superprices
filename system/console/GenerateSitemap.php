77b48f3a767c773111204420e4f273d282898f6a
VaHHiLLiH <serj.solo1488@gmail.com>
2022-11-24 16:38:52 +0300

    Скрипт генерации sitemap

---
+++ system/console/GenerateSitemap.php
@@ -0,0 +1,27 @@
+<?php
+
+use Services\{
+    ProductSitemapGenerate,
+    CategorySitemapGenerate,
+    ManufacturerSitemapGenerate,
+};
+
+$path = '/var/www/www-root/data/www/everydayshop.ru/';
+
+// Configuration
+require_once($path . 'config.php');
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+$product = new ProductSitemapGenerate();
+
+$product->generate($product->getDate(), $path.'sitemap/productsitemap.xml', 'product');
+
+$category = new CategorySitemapGenerate();
+
+$category->generate($category->getDate(), $path.'sitemap/categorysitemap.xml', 'category');
+
+$manufacturer = new ManufacturerSitemapGenerate();
+
+$manufacturer->generate($manufacturer->getDate(), $path.'sitemap/manufacturersitemap.xml', 'brand');
